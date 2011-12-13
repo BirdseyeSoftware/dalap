c19bcd656a5280dcd00197d9adadcec4f1f4c1fd
Tavis Rudd <tavis@damnsimple.com>
2011-12-12 17:45:40 -0800

    misc refactoring of Walker.state-map

--- src/seri/html.clj
+++ src/seri/html.clj
@@ -63,10 +63,11 @@
      (apply escape-html (flatten (seri.walk/walk x visitor-fn)))))
 
 ;;; test
-
+ 
 #_(to-html
    [" match tag: " [:p "Paragraph&" [1 2 "34" 'bar] " Foo"]
     " match tag: " [:p {:id "bar" :ti&tle "fo\"o"} "Paragraph" " Foo"]
+    " nesting: " [:div [:div [:div [:div]]]]
     " match tag: " [:input]
     " match tag: " [:input {:type "text"}]
     " match tag: " [:form#contact {:action "GET"}
@@ -77,5 +78,4 @@
     " safe: " (apply safe (keys html-esc-map))
     " chars to esc: " (keys html-esc-map)
     (apply str (keys html-esc-map))
-    ]
-   )
+    ])
--- src/seri/walk.clj
+++ src/seri/walk.clj
@@ -1,7 +1,6 @@
 (ns seri.walk)
 (defprotocol IWalker
-  (new-state [this st])
-  (update-state [this keys fn])
+  (update-in-state [this keys fn])
   (get-state [this]))
 
 (deftype Walker
@@ -12,23 +11,31 @@
    both single and dual arity versions of `serialize` for each
    visitable type."
 
-    [visitor-fn state]  
+    [visitor state-map]  
   
     clojure.lang.IFn
-    (invoke [this x] (visitor-fn x this))
+    (invoke [this x] (visitor x this))
     (applyTo [this args] (clojure.lang.AFn/applyToHelper this args))
 
     IWalker
-    (new-state [this st] (Walker. visitor-fn st))
-    (update-state [this ks fn]
+    (update-in-state [this ks fn]
       (let [keys (if (sequential? ks) ks [ks])]
-        (new-state this (update-in (.state this) keys fn))))
-    (get-state [this] state))
+        (Walker. visitor (update-in state-map keys fn))))
+    (get-state [this] state-map)
+
+    clojure.lang.ILookup
+    (valAt [this key] (state-map key))
+    (valAt [this key not-found] (state-map key not-found)))
 
 (defn walk [x visitor] ((Walker. visitor {}) x))
 
-(defn indent [w]
-  (update-state w ::indent #(inc (or % 0))))
+(defn indent
+  "This is an example of state manip functions that can used with the
+  Walker's state-map. Call (::indent w) inside a visitor to get the
+  current indent level and do some pretty printing with it."
+
+  [w]
+  (update-in-state w ::indent #(inc (or % 0))))
 
 ;; (defn walk
 ;;   [x visitor]  
