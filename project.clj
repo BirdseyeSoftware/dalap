37b2161b99dbd830840b69b04b95d95fa29b3f7f
Tavis Rudd <tavis@damnsimple.com>
2012-01-09 15:30:48 -0800

    added a note about the name

--- project.clj
+++ project.clj
@@ -1,4 +1,7 @@
 (defproject dalap "0.0.1-SNAPSHOT"
-  :description "Extremely flexible serialization library for Clojure"
+  :description "Extremely flexible serialization library for Clojure.
+The name is the acronym for Decide As Late As Possible, from lean
+programming."
+
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/core.match "0.2.0-alpha8"]])
