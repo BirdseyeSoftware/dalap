63a53abce1acecaf5daa36a75222c661bc9c3b81
Tavis Rudd <tavis@damnsimple.com>
2012-02-08 11:31:24 -0800

    tweak description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject dalap "0.0.1-SNAPSHOT"
-  :description "Extremely flexible serialization library for Clojure.
+  :description "Extremely flexible serialization / template library for Clojure.
 The name is the acronym for Decide As Late As Possible, from lean
 programming."
 
@@ -7,4 +7,3 @@ programming."
                  [org.clojure/core.match "0.2.0-alpha8"]]
 
   :repositories {"snapshots" {:url "s3p://lein-snapshots/snapshots"}})
-
