66ffd458eb5fb126429641d12e4f58a403deb48f
Roman Gonzalez <romanandreg@gmail.com>
2012-07-09 11:42:03 -0700

    Upgrading to clojure 1.4

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
 The name is the acronym for Decide As Late As Possible, from lean
 programming."
 
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/core.match "0.2.0-alpha8"]]
 
   :repositories {"snapshots" {:url "s3p://lein-snapshots/snapshots"}})
