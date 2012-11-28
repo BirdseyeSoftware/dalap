da1bba26d53739bb5628c19dcaa28d497d28d6a1
Roman Gonzalez <romanandreg@gmail.com>
2012-11-28 00:28:56 +0000

    Move dalap rules to it's own config file

---
+++ dalap_rules.clj
@@ -0,0 +1,13 @@
+[
+ [["src/clj/dalap/walk.clj" "src/cljs/dalap/walk.cljs"]
+  []]
+
+ [["src/clj/dalap/rules.clj" "src/cljs/dalap/rules.cljs"]
+   []]
+
+ [["test/clj/dalap/test/rules_test.clj" "test/cljs/dalap/rules_test.cljs"]
+  []]
+
+ [["test/clj/dalap/test/walk_test.clj" "test/cljs/dalap/walk_test.cljs"]
+   []]
+ ]
--- project.clj
+++ project.clj
@@ -7,53 +7,44 @@ programming."
                  [buster-cljs "0.1.0-SNAPSHOT"]]
 
   :plugins [[lein-cljsbuild "0.2.9"]
-            [lein-dalap-cljsbuild "0.1.0-SNAPSHOT"]]
-  :hooks [leiningen.dalap-cljsbuild]
+            [lein-dalap "0.1.0-SNAPSHOT"]]
+
+  :hooks [leiningen.dalap]
+
+  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.1"]]}}
 
   :source-paths ["src/clj" "src/cljs"]
   :test-paths ["test/clj" "test/cljs"]
 
   :cljsbuild
   {:builds
-   [{:id :dev
+   [{:id "dev"
      :source-path "src/cljs"
-     :dalap
-     {:paths
-      {"src/clj/dalap/walk.clj" "src/cljs/dalap/walk.cljs"
-       "src/clj/dalap/rules.clj" "src/cljs/dalap/rules.cljs"}}
      :compiler
      {:output-to "resources/js/dalap_core_dev.js"
       :optimizations :whitespace
       :pretty-print true}}
     ;;;
-    {:id :browser-test
+    {:id "browser-test"
      :source-path "test/cljs"
-     :dalap
-     {:paths
-      {"test/clj/dalap/test/rules_test.clj" "test/cljs/dalap/test/rules_test.cljs"
-       "test/clj/dalap/test/walk_test.clj" "test/cljs/dalap/test/walk_test.cljs" }}
      :compiler
      {:externs ["externs/buster.js"]
-      :libraries ["resources/js/dalap_core_dev.js"] ;;["src/cljs"]
+      :libraries ["resources/js/dalap_core_dev.js"]
       :output-to "resources/js/dalap_core_browser_test.js"
       :optimizations :simple
       :pretty-print true}}
     ;;;
-    {:id :node-test
+    {:id "node-test"
      :source-path "test/cljs"
      :compiler
      {:externs ["externs/buster.js"]
-      :libraries ["resources/js/dalap_core_dev.js"] ;;["src/cljs"]
+      :libraries ["resources/js/dalap_core_dev.js"]
       :output-to "resources/js/dalap_core_node_test.js"
       :optimizations :simple
       :pretty-print true}}
     ;;;
-    {:id :browser-test-optimized
+    {:id "browser-test-optimized"
      :source-path "test/cljs"
-     ;; :dalap
-     ;; {:paths
-     ;;  {"test/clj/dalap/test/rules_test.clj" "test/cljs/dalap/test/rules_test.cljs"
-     ;;   "test/clj/dalap/test/walk_test.clj" "test/cljs/dalap/test/walk_test.cljs"}}
      :compiler
      {:externs ["externs/buster.js"]
       :libraries ["resources/js/dalap_core_dev.js"] ;; ["src/cljs"]
